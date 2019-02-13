d1689781d10ab66a12dffa0c9e0d5ac82446e4cf
Devendra Malviya <devema12@in.ibm.com>
2019-02-13 12:59:10 +0530

    MSAutomation Change

--- app.py
+++ app.py
@@ -1,44 +1,115 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
+from xlrd import open_workbook
+from scp import SCPClient
+import time
 
-########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'your item here']
+ 
+import paramiko
+import sys
 
-bitterness = go.Bar(
-    x=beers,
-    y=[35, 60, 85, 75],
-    name='IBU',
-    marker={'color':'lightgreen'}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=[5.4, 7.1, 9.2, 4.3],
-    name='ABV',
-    marker={'color':'pink'}
-)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = 'Beer Comparison'
-)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+#all imports are here
 
-########### Display the chart
+#app = dash.Dash(name)
 
-app = dash.Dash()
 server = app.server
 
+nbytes = 4096
+hostname = 'RSCTBDEV1.fyre.ibm.com'
+port = 22
+username = 'root'
+password = 'Mstbsep20!8'
+remotefile="/tmp/MonitoringDailyReport/OutputAppend"+time.strftime('%Y%m%d')+".xls"
+file="OutputAppend"+time.strftime('%Y%m%d')+".xls"
+
+
+ssh = paramiko.SSHClient()
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname, username="root", password="Mstbsep20!8")
+print("Connected")
+scp = SCPClient(ssh.get_transport())
+scp.get(remotefile)
+scp.close()
+
+wb = open_workbook(file)
+createOrder=[]
+ScheduledOrder=[]
+ReleaseOrder=[]
+ShippedOrder=[]
+CanceledOrder=[]
+ReturnCreated=[]
+
+for sheet in wb.sheets():
+    number_of_rows = sheet.nrows
+    number_of_columns = sheet.ncols
+    time=[]
+
+    items = []
+
+    rows = []
+    for col in range(1, number_of_columns):
+        values = []
+        for row in range(0, number_of_rows):
+            value = (sheet.cell(row, col).value)
+            try:
+                if(row==0):
+                    time.append(value)
+                if(row==1):
+                    createOrder.append(value);
+                if(row==2):
+                    ScheduledOrder.append(value);
+                if(row==3):
+                    ReleaseOrder.append(value)
+                if (row == 4):
+                    ShippedOrder.append(value)
+                if(row== 5):
+                    CanceledOrder.append(value)
+                if(row ==6 ):
+                    ReturnCreated.append(value)
+
+               # switch_func(row,value)
+               # value = str(int(value))
+                #print value
+            except ValueError:
+                pass
+            finally:
+                values.append(value)
+
+print 'Done'
+
+
+
+
+
+
+
 app.layout = html.Div(children=[
-    html.H1('something completely different'),
+    html.H1(children='MS Automation Dash'),
+html.Div(id='live-update-text'),
+        #dcc.Graph(id='live-update-graph'),
     dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    )]
-)
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': time, 'y': createOrder, 'type': 'line', 'name': 'CreateOrder', 'id': 'live-update-graph'},
+                {'x': time, 'y': ReleaseOrder, 'type': 'line', 'name': 'ReleaseOrder'},
+                {'x': time, 'y': CanceledOrder, 'type': 'line', 'name': 'CanceledOrder'},
+                {'x': time, 'y': ReturnCreated, 'type': 'line', 'name': 'ReturnCreated'},
+
+            ],
+            'layout': {
+                'title': 'MS Automation DashBoard'
+            }
+        }
+    )
+])
 
 if __name__ == '__main__':
-    app.run_server()
+  app.run_server(debug=True)
+
